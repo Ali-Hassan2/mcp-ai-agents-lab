98f22f96d2a8eab09edbf508c678cf1e0b7b899d
Ali Hassan <alihassan26032004@gmail.com>
2025-11-27 16:19:52 +0500

    Connection mcp-client & mcp-server

--- lab-2_(X_Posting_Agent)/mcp-client/index.js
+++ lab-2_(X_Posting_Agent)/mcp-client/index.js
@@ -3,6 +3,7 @@ import readline from "readline/promises"
 import { GoogleGenAI } from "@google/genai"
 import { Client } from "@modelcontextprotocol/sdk/client/index.js"
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js"
+import { property, toLowerCase } from "zod"
 dotenv.config()
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_KEY,
@@ -12,7 +13,7 @@ const mcpClient = new Client({
   name: "mcp-client-x",
   version: "1.0.0",
 })
-
+let tools = []
 const chatHistory = []
 const rl = readline.createInterface({
   input: process.stdin,
@@ -23,4 +24,52 @@ mcpClient
   .connect(
     new StreamableHTTPClientTransport(new URL("http://localhost:3000/mcp"))
   )
-  .then(async () => {})
+  .then(async () => {
+    console.log("CONNECTION ESTABLISHED BETWEEN MCP_SERVER AND CLIENT.")
+    chatLoop()
+    const toolsFromServer = (await mcpClient.listTools()).tools.map((tool) => {
+      return {
+        name: tool.name,
+        description: tool.description,
+        parameters: {
+          type: tool.inputSchema.type,
+          properties: tool.inputSchema.properties,
+          required: tool.inputSchema.required,
+        },
+      }
+    })
+    tools.append(toolsFromServer)
+    console.log("Available Tools are:", tools)
+  })
+
+async function chatLoop() {
+  const question = await rl.question("Ask: ")
+
+  chatHistory.push({
+    role: "user",
+    parts: [
+      {
+        text: question,
+        type: "text",
+      },
+    ],
+  })
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    content: chatHistory,
+    
+  })
+
+  const responseText = response.candidates[0].content.parts[0].text
+  chatHistory.push({
+    role: "model",
+    parts: [
+      {
+        text: responseText,
+        type: "text",
+      },
+    ],
+  })
+  console.log(`LLM: ${responseText}`)
+  chatLoop()
+}
